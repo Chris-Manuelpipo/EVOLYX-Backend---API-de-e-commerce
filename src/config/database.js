296c26ace6a0ffbd0b2f7778b21de3ec384f0f98
Chris Manuel <etchomechris2000@gmail.com>
2026-02-19 20:34:01 +0100

    Modification de config/database.js pour connexion bd sur render

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,25 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// module.exports = pool;
+
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // nécessaire pour Render
+  }
 });
 
 module.exports = pool;
